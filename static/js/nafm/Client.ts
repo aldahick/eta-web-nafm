ee46186385e072060bde9717d43c73c17df76211
Alex Hicks <aldahick@iupui.edu>
2017-12-06 08:33:30 -0500

    Export Vector2 and Direction to client

--- static/js/nafm/Client.ts
+++ static/js/nafm/Client.ts
@@ -1,3 +1,4 @@
+import * as engine from "nafm/engine.js";
 import io from "socket.io-client";
 import moment from "moment";
 import ChatMessage from "../../../lib/ChatMessage";
@@ -53,19 +54,12 @@ export default class ClientGame {
         if ((<KeyboardEvent>evt.originalEvent).repeat) {
             return;
         }
-        let direction: Direction;
-        if (evt.which === JQuery.Key.ArrowUp || evt.which === JQuery.Key.W) direction = Direction.Up;
-        if (evt.which === JQuery.Key.ArrowDown || evt.which === JQuery.Key.S) direction = Direction.Down;
-        if (evt.which === JQuery.Key.ArrowLeft || evt.which === JQuery.Key.A) direction = Direction.Left;
-        if (evt.which === JQuery.Key.ArrowRight || evt.which === JQuery.Key.D) direction = Direction.Right;
+        let direction: engine.Direction;
+        if (evt.which === JQuery.Key.ArrowUp || evt.which === JQuery.Key.W) direction = engine.Direction.Up;
+        if (evt.which === JQuery.Key.ArrowDown || evt.which === JQuery.Key.S) direction = engine.Direction.Down;
+        if (evt.which === JQuery.Key.ArrowLeft || evt.which === JQuery.Key.A) direction = engine.Direction.Left;
+        if (evt.which === JQuery.Key.ArrowRight || evt.which === JQuery.Key.D) direction = engine.Direction.Right;
         if (direction === undefined) return;
         this.socket.emit("move", direction);
     }
-}
-
-enum Direction {
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3
 }
