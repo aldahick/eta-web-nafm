02143abf0e3152c93f30bc0bfa36345bfa185c62
Brian Huntley <bboy300@comcast.net>
2017-12-06 18:20:49 -0500

    Merge conflicts

--- lib/Server.ts
+++ lib/Server.ts
@@ -18,9 +18,10 @@ export class Server {
         instance = this;
         this.uid = eta.crypto.generateSalt();
         this.game = new engine.Game();
-        this.generateMap();
         this.io = io;
         this.io.on("connection", socket => this.onConnect(<any>socket).catch(err => eta.logger.error(err)));
+        engine.Enemy.load().then(() => this.generateMap())
+            .catch(err => eta.logger.error(err));
     }
 
     public close(): void {
