2478f4cf99161b5a638bef7f2b168ceb64cb5a4b
Alex Hicks <alex@alexhicks.net>
2017-12-19 16:14:05 -0500

    Port to new ILifecycleHandler structure

--- lifecycle/SocketIOLifecycle.ts
+++ lifecycle/SocketIOLifecycle.ts
@@ -1,12 +1,18 @@
 import * as eta from "../eta";
 import * as express from "express";
 import * as SocketIOServer from "socket.io";
+import WebServer from "../../../server/WebServer";
 import { Server } from "../lib/Server";
 import * as lib from "../lib/index";
 
 export default class SocketIOLifecycle extends eta.ILifecycleHandler {
-    public async beforeServerStart(): Promise<void> {
-        const io: SocketIO.Server = SocketIOServer(this.server.server, {
+    public register(server: WebServer): void {
+        server.on("pre-server-start", () => this.preServerStart(server));
+        server.on("server-stop", this.onServerStop.bind(this));
+    }
+
+    public async preServerStart(server: WebServer): Promise<void> {
+        const io: SocketIO.Server = SocketIOServer(server.server, {
             path: "/socket.io/hicks-web-nafm",
             serveClient: false
         });
